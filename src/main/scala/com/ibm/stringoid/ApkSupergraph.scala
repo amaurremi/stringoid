094fc3dba006683ea1d053dfe07cea94a97bafdb
Marianna Rapoport <mrapoport@uwaterloo.ca>
2015-06-23 00:40:47 -0400

    redundant args

--- src/main/scala/com/ibm/stringoid/ApkSupergraph.scala
+++ src/main/scala/com/ibm/stringoid/ApkSupergraph.scala
@@ -3,7 +3,7 @@ package com.ibm.stringoid
 object ApkSupergraph {
 
   def main(args: Array[String]): Unit = {
-    val urls = Urls("cgeo.geocaching", distinct = true, nonEmptyCg = true)
+    val urls = Urls("cgeo.geocaching")
     urls.print()
   }
 }
