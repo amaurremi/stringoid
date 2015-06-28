8177ea5d0ff4cd063f40bac3e9e03188522edb4d
Marianna Rapoport <mrapoport@uwaterloo.ca>
2015-06-28 16:08:23 -0400

    working on cmd args (DOES NOT COMPILE YET)

--- src/main/scala/com/ibm/stringoid/AnalysisType.scala
+++ src/main/scala/com/ibm/stringoid/AnalysisType.scala
@@ -1,8 +1,21 @@
 package com.ibm.stringoid
 
-import com.ibm.stringoid.retrieve.{CgIrUrlRetriever, ChaIrUrlRetriever, GrepUrlRetriever, UrlRetriever}
+import com.ibm.stringoid.retrieve.{CgIrUrlRetriever, ChaIrUrlRetriever, GrepUrlRetriever}
+import scopt.Read
 
-sealed abstract class AnalysisType(val retriever: UrlRetriever, val name: String)
-case object CgIrAnalysis extends AnalysisType(CgIrUrlRetriever, "CG_IR")
-case object ChaIrAnalysis extends AnalysisType(ChaIrUrlRetriever, "CHA_IR")
-case object GrepAnalysis extends AnalysisType(GrepUrlRetriever, "GREP")
+object AnalysisType extends Enumeration {
+  type AnalysisType = Value
+  val CgIr, ChaIr, Grep = Value
+
+  def retriever(at: AnalysisType) = at match {
+    case CgIr  =>
+      CgIrUrlRetriever
+    case ChaIr =>
+      ChaIrUrlRetriever
+    case Grep  =>
+      GrepUrlRetriever
+  }
+
+  implicit val analysisTypeRead: scopt.Read[AnalysisType] =
+    Read.reads(AnalysisType.withName)
+}
--- src/main/scala/com/ibm/stringoid/Main.scala
+++ src/main/scala/com/ibm/stringoid/Main.scala
@@ -2,6 +2,9 @@ package com.ibm.stringoid
 
 import java.nio.file.{Files, Path, Paths}
 
+import com.ibm.stringoid.AnalysisType._
+import scopt.{Read, OptionParser}
+
 import scala.collection.JavaConversions._
 
 object Main {
@@ -9,13 +12,21 @@ object Main {
   private[this] val RESOURCES_PATH: Path = Paths.get("src", "test", "resources")
 
   def main(args: Array[String]): Unit = {
+    // parser.parse returns Option[C]
+    parser.parse(args, CmdOptions()) map { options =>
+      ???
+    } getOrElse {
+      ???
+      // arguments are bad, usage message will have been displayed
+    }
+
     if (args.isEmpty)
-      runPlaydroneApks(CgIrAnalysis, GrepAnalysis, "playdrone_apks")
+      runPlaydroneApks(CgIr, Grep, "playdrone_apks")
     else
       runSingleApk(???, ???, ???)
   }
 
-  def runPlaydroneApks(a1: AnalysisType, a2: AnalysisType, apkDir: String) = {
+  def runPlaydroneApks(a1: AnalysisType, a2: AnalysisType, apkDir: String, outDir: String) = {
     val apkDirPath = Paths.get(RESOURCES_PATH.toString, apkDir)
     Files.newDirectoryStream(apkDirPath, "*.apk") foreach {
       file =>
@@ -30,13 +41,44 @@ object Main {
   /**
    * Write the output of Urls to specified file
    */
-  def write(comparison: Compare, apkPath: Path, outDir: Option[Path] = None): Unit = {
-    val od = outDir.getOrElse(Paths.get("target", "url_comparison")).normalize()
-    Files.createDirectories(od)
-    val name1: String = comparison.a1Urls.analysisType.name
-    val name2: String = comparison.a1Urls.analysisType.name
+  def write(comparison: Compare, apkPath: Path, outDir: Path): Unit = {
+    Files.createDirectories(outDir)
+    val name1: String = comparison.a1Urls.analysisType.toString
+    val name2: String = comparison.a1Urls.analysisType.toString
     val logName = "%s_%s_%s.txt".format(apkPath.getFileName.toString, name1, name2)
-    val logPath = Paths.get(od.toString, logName)
+    val logPath = Paths.get(outDir.toString, logName)
     Files.write(logPath, Seq(comparison.toString))
+  }
+
+  case class CmdOptions(
+    analysis1: AnalysisType = Grep,
+    analysis2: AnalysisType = CgIr,
+    outDir: Path            = Paths.get("target", "url_comparison"),
+    files: Seq[Path]        = Seq.empty[Path]
+  )
+
+  val parser = new OptionParser[CmdOptions]("scopt") {
+    implicit val pathRead: scopt.Read[Path] =
+      Read.reads(
+        s =>
+          Paths.get(s, Seq.empty[String]: _*))
+
+    head("stringoid")
+    opt[AnalysisType]("a1") required() valueName "<analysis1>" action {
+      (a1, opts) =>
+        opts.copy(analysis1 = a1)
+    } text s"first analysis type (${AnalysisType.values})"
+    opt[AnalysisType]("a2") required() valueName "<analysis2>" action {
+      (a1, opts) =>
+        opts.copy(analysis1 = a1)
+    } text s"second analysis type (${AnalysisType.values})"
+    opt[Path]('o', "outdir") optional() valueName "<out dir>" action {
+      (o, opts) =>
+        opts.copy(outDir = o)
+    } text "output directory"
+    arg[Path]("<file>...") unbounded() action {
+      (f, opts) =>
+        opts.copy(files = opts.files :+ f)
+    } text "APK files"
   }
 }
