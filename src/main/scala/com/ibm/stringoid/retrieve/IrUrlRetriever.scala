1948324985fabcfd5033c0c1e95ad77f1486db93
Marianna Rapoport <mrapoport@uwaterloo.ca>
2015-06-29 14:51:17 -0400

    optimizing imports

--- src/main/scala/com/ibm/stringoid/retrieve/IrUrlRetriever.scala
+++ src/main/scala/com/ibm/stringoid/retrieve/IrUrlRetriever.scala
@@ -2,11 +2,9 @@ package com.ibm.stringoid.retrieve
 
 import java.nio.file.Path
 
-import com.ibm.wala.classLoader.IMethod
 import com.ibm.wala.ssa.IR
 import edu.illinois.wala.ipa.callgraph.FlexibleCallGraphBuilder
 
-import scala.collection.JavaConversions._
 import scala.collection._
 
 /**
