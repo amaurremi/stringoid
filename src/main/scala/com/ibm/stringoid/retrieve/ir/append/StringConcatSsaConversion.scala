9b9c36614b8d94ca840974f5c22d0383a5ce78ad
Marianna Rapoport <mrapoport@uwaterloo.ca>
2015-07-01 10:56:25 -0400

    implementing part of ssa conversion api

--- src/main/scala/com/ibm/stringoid/retrieve/ir/append/StringConcatSsaConversion.scala
+++ src/main/scala/com/ibm/stringoid/retrieve/ir/append/StringConcatSsaConversion.scala
@@ -3,16 +3,17 @@ package com.ibm.stringoid.retrieve.ir.append
 import com.ibm.wala.cast.ir.ssa.AbstractSSAConversion
 import com.ibm.wala.ssa._
 
-import scala.collection.mutable
+import scala.collection.{breakOut, mutable}
 
 class StringConcatSsaConversion(ir: IR) extends AbstractSSAConversion(ir, new SSAOptions) {
 
   private[this] case class DefUse(df: Array[ValueNumber], uses: Array[ValueNumber])
 
-  private[this] val phiInstructions = mutable.Map.empty[SSACFG#BasicBlock, Set[SSAPhiInstruction]]
-  private[this] val defUses         = mutable.Map.empty[SSAInstruction, DefUse]
-  
-  private[this] var valNumIterator  = Iterator.from(getMaxValueNumber + 1)
+  private[this] val basicBlockToPhis =
+    mutable.Map.empty[SSACFG#BasicBlock, Array[SSAPhiInstruction]] withDefaultValue Array.empty[SSAPhiInstruction]
+  private[this] val defUses          = mutable.Map.empty[SSAInstruction, DefUse]
+
+  private[this] var valNumIterator   = Iterator.from(getMaxValueNumber + 1)
 
   /**
    * Does this instruction correspond to the creation of a new string, StringBuilder, or StringBuffer?
@@ -51,7 +52,7 @@ class StringConcatSsaConversion(ir: IR) extends AbstractSSAConversion(ir, new SS
 
   override def pushAssignment(inst: SSAInstruction, index: Int, newRhs: Int): Unit = {}
 
-  override def getPhi(B: SSACFG#BasicBlock, index: Int): SSAPhiInstruction = ???
+  override def getPhi(B: SSACFG#BasicBlock, index: Int): SSAPhiInstruction = basicBlockToPhis(B)(index)
 
   override def isAssignInstruction(inst: SSAInstruction): Boolean = false
   
@@ -59,30 +60,56 @@ class StringConcatSsaConversion(ir: IR) extends AbstractSSAConversion(ir, new SS
 
   override def isLive(Y: SSACFG#BasicBlock, V: ValueNumber): Boolean = true
 
-  override def getDef(inst: SSAInstruction, index: Int): Int = ???
+  override def getDef(inst: SSAInstruction, index: Int): Int = defUses(inst).df(index)
 
   override def repairPhiUse(
     BB: SSACFG#BasicBlock,
     phiIndex: Int,
     rvalIndex: Int,
     newRval: ValueNumber
-  ): Unit = ???
+  ): Unit = {
+    val oldInstr = basicBlockToPhis(BB)(phiIndex)
+    val oldUses1 = rvalIndex + 1 to oldInstr.getNumberOfUses map oldInstr.getUse
+    val oldUses2 = 0 to rvalIndex map oldInstr.getUse
+    val newUses  = ((oldUses1 :+ newRval) ++ oldUses2)(breakOut)
+    basicBlockToPhis(BB)(phiIndex) setValues newUses
+  }
 
-  override def setPhi(B: SSACFG#BasicBlock, index: Int, inst: SSAPhiInstruction): Unit = ???
+  override def setPhi(B: SSACFG#BasicBlock, index: Int, inst: SSAPhiInstruction): Unit =
+    basicBlockToPhis(B)(index) = inst
 
-  override def placeNewPhiAt(value: ValueNumber, Y: SSACFG#BasicBlock): Unit = ???
+  override def placeNewPhiAt(value: ValueNumber, Y: SSACFG#BasicBlock): Unit = {
+    val params: Array[ValueNumber] = Array[ValueNumber](CFG getPredNodeCount Y)
+    0 to params.length foreach { params(_) = value }
 
-  override def repairPhiDefs(phi: SSAPhiInstruction, newDefs: Array[ValueNumber]): SSAPhiInstruction = ???
+    val phi     = new SSAPhiInstruction(SSAInstruction.NO_INDEX, value, params) // todo is no index correct?
+    val oldPhis = basicBlockToPhis(Y)
+    val newPhis = new Array[SSAPhiInstruction](oldPhis.length + 1)
+    oldPhis copyToArray newPhis
 
-  override def getNumberOfDefs(inst: SSAInstruction): Int = ???
+    basicBlockToPhis += (Y -> newPhis)
+  }
 
-  override def getNumberOfUses(inst: SSAInstruction): Int = ???
+  override def repairPhiDefs(phi: SSAPhiInstruction, newDefs: Array[ValueNumber]): SSAPhiInstruction =
+    phi.copyForSSA(ir.getMethod.getDeclaringClass.getClassLoader.getInstructionFactory, newDefs, null).asInstanceOf[SSAPhiInstruction] // todo correct?
 
-  override def repairExit(): Unit = ???
+  override def getNumberOfDefs(inst: SSAInstruction): Int =
+    defUses get inst match {
+      case Some(defUse) => defUse.df.length
+      case None         => ???
+    }
+
+  override def getNumberOfUses(inst: SSAInstruction): Int =
+    defUses get inst match {
+      case Some(defUse) => defUse.uses.length
+      case None         => ???
+    }
+
+  override def repairExit(): Unit = {}
 
   override def popAssignment(inst: SSAInstruction, index: Int): Unit = {}
 
-  override def getUse(inst: SSAInstruction, index: Int): Int = ???
+  override def getUse(inst: SSAInstruction, index: Int): Int = defUses(inst).uses(index)
 
   override def isConstant(vn: ValueNumber): Boolean = ir.getSymbolTable isConstant vn
 }
