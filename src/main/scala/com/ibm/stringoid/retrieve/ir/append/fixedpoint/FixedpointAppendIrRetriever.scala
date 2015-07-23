999a5e16b10a4f3a19b7b3aa175c27a4f3b345bb
Marianna Rapoport <mrapoport@uwaterloo.ca>
2015-07-23 13:30:05 -0400

    making it compile

--- src/main/scala/com/ibm/stringoid/retrieve/ir/append/fixedpoint/FixedpointAppendIrRetriever.scala
+++ src/main/scala/com/ibm/stringoid/retrieve/ir/append/fixedpoint/FixedpointAppendIrRetriever.scala
@@ -3,10 +3,14 @@ package com.ibm.stringoid.retrieve.ir.append.fixedpoint
 import java.nio.file.Path
 
 import com.ibm.stringoid.retrieve.ir.IrFromBuilderRetriever
-import com.ibm.stringoid.retrieve.ir.append.AppendUrl
-import com.ibm.wala.ssa.IR
+import com.ibm.stringoid.retrieve.ir.append.{AppendUrl, ValueNumber}
+import com.ibm.wala.ssa.{IR, SymbolTable}
 
-object FixedpointAppendIrRetriever extends IrFromBuilderRetriever with AppendUrl with AbstractStringBuilderModule {
+object FixedPointAppendIrRetriever
+  extends IrFromBuilderRetriever
+  with AppendUrl
+  with AbstractStringBuilderModule
+  with StringAppendModule {
 
   override type Url = UrlSeq
 
@@ -23,8 +27,48 @@ object FixedpointAppendIrRetriever extends IrFromBuilderRetriever with AppendUrl
     UrlsWithSources(urlWithSourcesMap)
   }
 
-  private[this] def getUrlsForIr(ir: IR): Seq[Url] = {
-    val valNumToAsbo = valNumToAbstractStringBuilder(ir)
-    ???
+  private[this] def getUrlsForIr(ir: IR): Set[Url] = {
+    val valNumToAsbo = valueNumberToAsbo(asboSolver(ir))
+    val stringAppends: Set[AltStringConcatenation] = stringAppendsAtEndOfMethod(ir, valNumToAsbo)
+    val strings: Set[SingleStringConcatenation] = stringAppends flatMap { _.flatten }
+    // identifying URLs
+    val table = ir.getSymbolTable
+    strings collect {
+      case string if hasUrlPrefix(table, string) =>
+        UrlSeq(parseUrl(table, string))
+    }
+  }
+
+  // todo make lazy (avoid flattening of unnecessary data structures)
+  private[this] def hasUrlPrefix(table: SymbolTable, string: SingleStringConcatenation): Boolean = {
+    def matchesUrlPrefix(vn: ValueNumber) =
+      (table isConstant vn) && (table getStringValue vn matches URL_PREFIX)
+    string match {
+      case SingleAppendArgument(vn) =>
+        matchesUrlPrefix(vn)
+      case SingleStringSeq(s :: _)                              =>
+        // todo trim empty strings?
+        hasUrlPrefix(table, s)
+      case _                                                    =>
+        false
+    }
+  }
+
+  private[this] def parseUrl(table: SymbolTable, string: SingleStringConcatenation): Seq[UrlPart] = {
+    string match {
+      case SingleStringSeq(strings) =>
+        // todo add accumulator to method and do tail recursion
+        strings.foldLeft(Seq.empty[UrlPart]) {
+          case (prefix, s) =>
+            prefix ++ parseUrl(table, s)
+        }
+      case SingleAppendArgument(vn) =>
+        // todo we could add more URL-part-types and be more precise about the type of value
+        if (table isConstant vn)
+          Seq(UrlString((table getConstantValue vn).toString))
+        else
+          Seq(UrlPlaceHolder)
+      // todo there is also the case where the append argument is a StringBuilder! This is not handled currently!
+    }
   }
 }
