790a13b2bc0fbe2ac2598000b6f07db931abe4fd
Marianna Rapoport <mrapoport@uwaterloo.ca>
2015-10-01 16:21:00 -0400

    what we have and what we want

---
+++ src/main/java/com/ibm/stringoid/Example.java
@@ -0,0 +1,50 @@
+package com.ibm.stringoid;
+
+public class Example {
+
+    /**
+     * Actual result:
+     * "http://domain.com?" + [ ] + "key2=val2"
+     *
+     * Note that the String.format analysis will not return anything because the formatted string
+     * does not have a URL prefix.
+     *
+     * Desired result:
+     * "http://domain.com?" + "key" + "=" + "val," + "key2=val2"
+     */
+    void wrongStringFormat() {
+        String query = String.format("%s=%s,", "key", "val");
+        StringBuilder urlBuilder = new StringBuilder();
+        urlBuilder.append("http://domain.com?");
+        urlBuilder.append(query);
+        urlBuilder.append("key2=val2");
+    }
+
+    /**
+     * Actual result:
+     * "http://domain.com?" + [ ]
+     *
+     * Desired result:
+     * "http://domain.com?" + "key=val"
+     */
+    void wrongStringBuilderToString() {
+        StringBuilder queryBuilder = new StringBuilder();
+        queryBuilder.append("key=val");
+        String url = "http://domain.com?" + queryBuilder.toString();
+    }
+
+    /**
+     * Actual result:
+     * "http://domain.com?" + [ ]
+     *
+     * Desired result:
+     * "http://domain.com?" + "key=val"
+     */
+    void wrongInterProc() {
+        String url = "http://domain.com?" + getQuery();
+    }
+
+    String getQuery() {
+        return "key=val";
+    }
+}
