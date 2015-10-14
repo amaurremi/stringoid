f62889471a8f1a9ee583184407e92ebcf00d386c
Marianna Rapoport <mrapoport@uwaterloo.ca>
2015-10-14 19:18:28 -0400

    enabling string.format in test

--- src/test/java/testPrograms/UrlConcatenation.java
+++ src/test/java/testPrograms/UrlConcatenation.java
@@ -4,8 +4,7 @@ public class UrlConcatenation {
 
     public static void main(String[] args) {
         String queryParam2 = "query2";
-        String query = new StringBuilder("key1=query1,key2=").append(queryParam2).toString();
-//        String query = String.format("key1=%s,key2=%s", "query1", queryParam2);
+        String query = String.format("key1=%s,key2=%s", "query1", queryParam2);
         StringBuilder sb = new StringBuilder("http://");
         sb.append("domain.com?");
         sb.append(query);
