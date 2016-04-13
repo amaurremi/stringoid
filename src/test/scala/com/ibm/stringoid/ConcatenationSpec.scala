1dd2f14d2b6179b12a94df186b040dbca42869fe
Marianna Rapoport <mrapoport@uwaterloo.ca>
2016-04-13 11:25:18 -0400

    uncomment

--- src/test/scala/com/ibm/stringoid/ConcatenationSpec.scala
+++ src/test/scala/com/ibm/stringoid/ConcatenationSpec.scala
@@ -38,19 +38,19 @@ class ConcatenationSpec extends FunSpec with StringoidAnalysis {
 //      run(analysisConfig)
 //    }
 
-//    /* INTRA procedural*/
-//    it("computes URLs in CHA IR analysis") {
-//      val file = Paths.get("src/test/java/intraproc")
-//      val analysisConfig = AnalysisConfig(irSource = Cha, ignoreLibs = true, analysis = Append, file = file, outputUrls = true)
-//      run(analysisConfig)
-//    }
+    /* INTRA procedural*/
+    it("computes URLs in CHA IR analysis") {
+      val file = Paths.get("src/test/java/intraproc")
+      val analysisConfig = AnalysisConfig(irSource = Cha, ignoreLibs = true, analysis = Append, file = file, outputUrls = true)
+      run(analysisConfig)
+    }
 
-//    /* INTER procedural */
-//    it("computes URLs in inter-procedural analysis") {
-//      val file = Paths.get("src/test/java/interproc")
-//      val analysisConfig = AnalysisConfig(irSource = InterProc, ignoreLibs = true, analysis = Append, file = file, outputUrls = true)
-//      run(analysisConfig)
-//    }
+    /* INTER procedural */
+    it("computes URLs in inter-procedural analysis") {
+      val file = Paths.get("src/test/java/interproc")
+      val analysisConfig = AnalysisConfig(irSource = InterProc, ignoreLibs = true, analysis = Append, file = file, outputUrls = true)
+      run(analysisConfig)
+    }
 
     def run(config: AnalysisConfig) = {
       val interProc = config.irSource == IrSource.InterProc
